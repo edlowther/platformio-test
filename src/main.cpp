a160197d495654986f4ecb31461e07d3153d0514
Ed Lowther <edlowther@users.noreply.github.com>
2022-09-30 16:56:08 +0100

    Implement increased separation on more loudness

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,25 @@ void setup() {
   Serial.begin(9600);
 }
 
+int loudness;
+float loudnessFactor; 
+
 void loop() {
+  loudness = analogRead(0);
+  Serial.println(loudness);
+  loudnessFactor = map(loudness, 0, 200, 100, 0);
+  if (loudnessFactor < 0) {
+    loudnessFactor = 0;
+  } else if (loudnessFactor > 100) {
+    loudnessFactor = 100;
+  }
+  if (loudnessFactor > 20) {
+    SEPARATION_STRENGTH = 0.9;
+  } else {
+    SEPARATION_STRENGTH = 0.5; 
+  }
+  Serial.println(loudnessFactor);
+  Serial.println("----");
   // Update the position of the flock: 
   flock.update();
   // Map the new positions to the pixels in the grid:
